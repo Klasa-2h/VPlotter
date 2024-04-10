818c07b685394a70aca227b8674b8ac4d837af7a
unknown <kacpergurzeda5@gmail.com>
2024-04-10 17:54:02 +0200

    dodalem funkcje w klasie ImageProcessor zamieniajaca obraz na czarnobialy

---
+++ src/image_processor.py
@@ -0,0 +1,13 @@
+from PIL import Image
+import numpy as np
+
+
+class ImageProcessor:
+    def __init__(self) -> None:
+        self.image = None
+
+    def read_image(self, image_path: str) -> None:
+        self.image = np.array(Image.open(image_path))
+    
+    def mono_image(self) -> None:
+        self.image = self.image.convert('1')
