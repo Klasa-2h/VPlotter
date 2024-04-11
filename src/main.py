4522915c81246d93f3816ff9725b39dbc46eb74a
Krzysztof Murawski <120591589+KrzysztofMurawski@users.noreply.github.com>
2024-04-11 20:07:50 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ image_path = os.path.join(resources_folder, image_file_name)
 def main() -> None:
     img_processor = ImageProcessor()
     img_processor.read_image(image_path)
+    img_processor.zmiana_obrazu()
     img_processor.mono_image()
 
     print(img_processor.image)
