fcf479e9edba4c04d55e2004c28e8f0324cf682c
KrzysMur <mure.krzys@gmail.com>
2024-04-22 18:36:35 +0200

    Zwracanie odległości między łukami

--- src/image_processor.py
+++ src/image_processor.py
@@ -28,7 +28,7 @@ class ImageProcessor:
     def mono_image(self) -> None:
         self.image = self.image.convert('L')
 
-    def podziel_na_luki(self):
+    def podziel_na_luki(self) -> tuple[float, list[Luk]]:
         r1 = round(math.sqrt(start_x ** 2 + (start_y + rozmiar_piksela / 2) ** 2), 3)
         r2 = round(math.sqrt(start_x ** 2 + (start_y + rozmiar_piksela / 2 + rozmiar_piksela) ** 2), 3)
         odleglosc_miedzy_lukami = round(r2 - r1, 2)
@@ -37,13 +37,16 @@ class ImageProcessor:
         luki = []
         luk = []
         czy_pusty = False
+
         while not czy_pusty:
             sx = None
             sy = None
             ex = None
             ey = None
+
             for i in range(len(img)):
                 for j in range(len(img[0])-1, -1, -1):
+
                     if (abs(r - math.sqrt((start_y + i*rozmiar_piksela + rozmiar_piksela/2) ** 2 +
                                           (start_x + j*rozmiar_piksela + rozmiar_piksela/2)**2))
                             <= odleglosc_miedzy_lukami/2):
@@ -59,11 +62,14 @@ class ImageProcessor:
 
             if len(luk) == 0:
                 czy_pusty = True
+
             else:
                 luki.append(Luk(luk[::-1], sx*rozmiar_piksela+start_x, sy*rozmiar_piksela+start_y, ex*rozmiar_piksela+start_x, ey*rozmiar_piksela+start_y, r))
+
             r += odleglosc_miedzy_lukami
             luk = []
-        return luki
+
+        return odleglosc_miedzy_lukami, luki
 
     def process_intensity_scale(self):
         gorny_limit = maksymalne_natezenie_barw
