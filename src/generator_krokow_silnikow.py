b2fd69a9dafe25cadb087352f4416381de3ccac4
KrzysMur <mure.krzys@gmail.com>
2024-05-31 18:57:29 +0200

    Rysowanie prostymi liniami

--- src/generator_krokow_silnikow.py
+++ src/generator_krokow_silnikow.py
@@ -11,46 +11,86 @@ def oblicz_dlugosc_prawego_sznurka(x, y):
     return math.sqrt((motor_spacing - x)**2 + y**2)
 
 
+def oblicz_dlugosc_lewego_sznurka(x, y):
+    return math.sqrt(x**2 + y**2)
+
+
 class GeneratorKrokowSilnikow:
-    def __init__(self, obraz: list[list[Pixcel]]):
+    def __init__(self, obraz: list[list[Pixcel]], rozmiar_piksela):
+        self.rozmiar_piksela = rozmiar_piksela
         self.obraz = obraz
         self.kroki = []
 
     def generuj(self):
-        for row in self.obraz[:-1:]:
-            for pixcel in row[:-1:]:
-                # Tutaj robimy ten dzyndzel od natezenia
-                dlugosc_dzyndzla = max_dlugosc_dzyndzli * (pixcel.natezenie/maksymalne_natezenie_barw)
-                ilosc_krokow_dzyndzla = odleglosc_na_kroki(dlugosc_dzyndzla)
+        for y in range(0, len(self.obraz[:-1:]), 2):
+            ymm = start_y + y * self.rozmiar_piksela
+            for x in range(ilosc_dzyndzli_poziomo):
+                # w tej petli idziemy w prawo
+                # obliczam miejsce na kartce gdzie jestesmy
+                xmm = start_x + x * odleglosc_miedzy_dzyndzlami
+                # rysuje dzyndzel
+                i, j = self.znajdz_piksel(xmm, ymm)
+                natezenie = self.obraz[j][i].natezenie
+                dlugosc_dzyndzla = natezenie / maksymalne_natezenie_barw * max_dlugosc_dzyndzli
+                self.kroki += (["11\n" for _ in range(odleglosc_na_kroki(dlugosc_dzyndzla))])
+                self.kroki += (["10\n" for _ in range(odleglosc_na_kroki(dlugosc_dzyndzla))])
 
-                self.kroki += ["11\n" for _ in range(ilosc_krokow_dzyndzla)]
-                self.kroki += ["10\n" for _ in range(ilosc_krokow_dzyndzla)]
-
-                # to nizej to na dodatkowy kontrast
-                self.kroki += ["00\n" for _ in range(ilosc_krokow_dzyndzla)]
-                self.kroki += ["01\n" for _ in range(ilosc_krokow_dzyndzla)]
-                # A potem idziemy do nastepnego piksela
-                lewy_do_rozwiniecia = abs(math.sqrt(pixcel.xmm**2+pixcel.ymm**2)
-                                          - math.sqrt(row[pixcel.x+1].xmm**2 + row[pixcel.x+1].ymm**2))
-                prawy_do_zwiniecia = abs(math.sqrt((motor_spacing-pixcel.xmm)**2 + pixcel.ymm**2)
-                                         - math.sqrt((motor_spacing - row[pixcel.x+1].xmm)**2 + row[pixcel.x+1].ymm**2))
+                # obliczam dlugosci sznurkow do przesuniecia do przejscia na kolejny dzyndzel
+                prawy_do_zwiniecia = (oblicz_dlugosc_prawego_sznurka(xmm, ymm)
+                                      - oblicz_dlugosc_prawego_sznurka(xmm + odleglosc_miedzy_dzyndzlami, ymm))
+                lewy_do_rozwiniecia = -(oblicz_dlugosc_lewego_sznurka(xmm, ymm)
+                                        - oblicz_dlugosc_lewego_sznurka(xmm + odleglosc_miedzy_dzyndzlami, ymm))
+                # zamieniam dlugosc na kroki
                 self.kroki += ["10\n" for _ in range(odleglosc_na_kroki(lewy_do_rozwiniecia))]
                 self.kroki += ["00\n" for _ in range(odleglosc_na_kroki(prawy_do_zwiniecia))]
+            # przechodze do nastepnej lini w dol
+            ymm += self.rozmiar_piksela
 
-            self.kroki.append("1\n")
-
-            p1 = row[-1]
-            p2 = self.obraz[row[0].y+1][1]
-
-            lewy_do_zwiniecia = math.sqrt(p1.xmm**2 + p1.ymm**2) - math.sqrt(p2.xmm**2 + p2.ymm**2)
-            prawy_do_rozwiniecia = abs(math.sqrt((motor_spacing - p1.xmm)**2 + p1.ymm**2)
-                                       - math.sqrt((motor_spacing - p2.xmm)**2 + p2.ymm**2))
-
-            self.kroki += ["11\n" for _ in range(odleglosc_na_kroki(lewy_do_zwiniecia))]
+            prawy_do_rozwiniecia = -(oblicz_dlugosc_prawego_sznurka(xmm, ymm - self.rozmiar_piksela)
+                                    - oblicz_dlugosc_prawego_sznurka(xmm, ymm))
+            lewy_do_rozwiniecia = -(oblicz_dlugosc_lewego_sznurka(xmm, ymm - self.rozmiar_piksela)
+                                   - oblicz_dlugosc_lewego_sznurka(xmm, ymm))
+            # zamieniam dlugosc na kroki
+            self.kroki += ["10\n" for _ in range(odleglosc_na_kroki(lewy_do_rozwiniecia))]
             self.kroki += ["01\n" for _ in range(odleglosc_na_kroki(prawy_do_rozwiniecia))]
 
-            self.kroki.append("0\n")
+            for x in range(ilosc_dzyndzli_poziomo - 1, -1, -1):
+                # w tej petli idziemy w lewo
+                xmm = start_x + x * odleglosc_miedzy_dzyndzlami
+                # rysuje dzyndzel
+                i, j = self.znajdz_piksel(xmm, ymm)
+                natezenie = self.obraz[j][i].natezenie
+                dlugosc_dzyndzla = natezenie / maksymalne_natezenie_barw * max_dlugosc_dzyndzli
+                self.kroki += (["11\n" for _ in range(odleglosc_na_kroki(dlugosc_dzyndzla))])
+                self.kroki += (["10\n" for _ in range(odleglosc_na_kroki(dlugosc_dzyndzla))])
 
+                prawy_do_rozwiniecia = -(oblicz_dlugosc_prawego_sznurka(xmm, ymm)
+                                         - oblicz_dlugosc_prawego_sznurka(xmm - odleglosc_miedzy_dzyndzlami, ymm))
+                lewy_do_zwiniecia = (oblicz_dlugosc_lewego_sznurka(xmm, ymm)
+                                     - oblicz_dlugosc_lewego_sznurka(xmm - odleglosc_miedzy_dzyndzlami, ymm))
+                self.kroki += ["01\n" for _ in range(odleglosc_na_kroki(prawy_do_rozwiniecia))]
+                self.kroki += ["11\n" for _ in range(odleglosc_na_kroki(lewy_do_zwiniecia))]
+
+            ymm += self.rozmiar_piksela
+
+            prawy_do_rozwiniecia = (oblicz_dlugosc_prawego_sznurka(xmm, ymm + self.rozmiar_piksela)
+                                    - oblicz_dlugosc_prawego_sznurka(xmm, ymm))
+            lewy_do_rozwiniecia = (oblicz_dlugosc_lewego_sznurka(xmm, ymm + self.rozmiar_piksela)
+                                   - oblicz_dlugosc_lewego_sznurka(xmm, ymm))
+
+            # zamieniam dlugosc na kroki
+            self.kroki += ["10\n" for _ in range(odleglosc_na_kroki(lewy_do_rozwiniecia))]
+            self.kroki += ["01\n" for _ in range(odleglosc_na_kroki(prawy_do_rozwiniecia))]
+
+
+
+    def znajdz_piksel(self, xmm, ymm):
+        ymm -= start_y
+        xmm -= start_x
+        ymm //= self.rozmiar_piksela
+        xmm //= odleglosc_miedzy_dzyndzlami
+        xmm = xmm/ilosc_dzyndzli_poziomo*len(self.obraz[0])
+        return int(xmm), int(ymm)
 
     def zapisz_do_pliku(self):
         with open(os.path.join(resources_folder, "kroki.txt"), "w") as file:
