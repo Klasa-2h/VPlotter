ce41a111e3ca0fd888ff7f912fcab7e1ff8da92f
KrzysMur <mure.krzys@gmail.com>
2024-10-03 13:52:28 +0200

    Naprawiono niektre bledy

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 
-image_path = "../res/zadanie2.png"
-steps_file_path = "../res/duzy_prostokat.txt"
+image_path = 'C:/Users/murek/Projects/VPlotter/res/obraz.jpg'
+steps_file_path = 'C:/Users/murek/Projects/VPlotter/res/Dane.txt'
 
 
 color_range = 8
@@ -10,7 +10,7 @@ final_image_width = 150
 starting_height_from_top = 500
 
 distance_between_motors = 1000
-motor_diameter = 37
+motor_diameter = 30
 motor_step_angle = 1.8
 
 simulation_line_thickness = 1
