8beab1f86404ae78ba4616ea3e00049778315526
KrzysMur <mure.krzys@gmail.com>
2024-12-08 17:46:11 +0100

    Pliki z danymi do drukowania

--- src/config.py
+++ src/config.py
@@ -1,24 +1,23 @@
 
 image_path = 'C:/Users/murek/Projects/VPlotter/res/Untitled.png'
-steps_file_path = 'C:/Users/murek/Projects/VPlotter/res/Dinopociung.txt'
+steps_file_path = 'C:/Users/murek/Projects/VPlotter/res/Test.txt'
 
 
 color_range = 8
 
 
-final_image_width = 150
-starting_height_from_top = 500
+final_image_width = 250
+starting_height_from_top = 300
 
 distance_between_motors = 1000
 motor_diameter = 30
 motor_step_angle = 1.8
-
 simulation_line_thickness = 1
 
 
 # VARIABLES FOR HORIZONTAL LINES DRAWING METHOD
-resolution_vertically = 50
-resolution_horizontally = 300
+resolution_vertically = 2
+resolution_horizontally = 10
 
 
 # VARIABLES FOR [...] DRAWING METHOD
